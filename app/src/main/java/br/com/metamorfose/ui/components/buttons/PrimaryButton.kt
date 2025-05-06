818960998f4d38f74856e6f3b99cfea0aa2f0352
Evelin Cordeiro <109860924+evamyuu@users.noreply.github.com>
2025-05-05 22:11:37 -0300

    feat: adjust PrimaryButton to accept customizable widths

--- app/src/main/java/br/com/metamorfose/ui/components/buttons/PrimaryButton.kt
+++ app/src/main/java/br/com/metamorfose/ui/components/buttons/PrimaryButton.kt
@@ -47,6 +47,7 @@ import br.com.metamorfose.ui.theme.WhiteLight
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 
@@ -114,7 +115,7 @@ fun PrimaryButton(
                     }
                 }
             }
-            .fillMaxWidth()
+            .width(width)
             .height(height)
             .background(
                 color = backgroundColor,
@@ -153,7 +154,8 @@ fun PrimaryButtonPreview() {
             backgroundColor = PurpleNormal,
             textColor = WhiteLight,
             strokeColor = PurpleNormal,
-            shadowColor = PurpleDarken
+            shadowColor = PurpleDarken,
+            width = 334.dp
         )
     }
 }
